bb9cc127d53767b1ad8bf29c0a7528453e9de62a
Daniel Tabaja <dtabaja@gmail.com>
2020-11-04 13:34:17 +0200

    first entities

---
+++ lib/Doctor.dart
@@ -0,0 +1,25 @@
+class Doctor{
+
+  String _userName;
+  String _passWord;
+  int _doctorId;
+  //Patient patient
+  //TreatmentType treatmentType
+
+  String get userName => _userName;
+  int get doctorId => _doctorId;
+  String get passWord => _passWord;
+
+  set doctorId(int value) {
+    _doctorId = value;
+  }
+
+  set passWord(String value) {
+    _passWord = value;
+  }
+
+  set userName(String value) {
+    _userName = value;
+  }
+
+}
