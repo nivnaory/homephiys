091f36b9a702eda575f4f29da99f5e06531bcd6e
Daniel Tabaja <daniel.tabaja@s.afeka.ac.il>
2020-12-23 00:42:03 +0200

    Exercise Page design completed

--- lib/ExercisePage.dart
+++ lib/ExercisePage.dart
@@ -9,15 +9,14 @@ class ExercisePage extends StatefulWidget {
   final Exercise exercise;
   final url = 'https://www.youtube.com/watch?v=tr6XsZVb-ZE';
   final title = 'Player';
+  final highScore = 13.0000;
 
   ExercisePage({@required this.exercise});
 
   _ExercisePage createState() => _ExercisePage();
 }
 
-
 class _ExercisePage extends State<ExercisePage> {
-
   YoutubePlayerController _controller;
 
   void runYoutubePlayer() {
@@ -27,8 +26,7 @@ class _ExercisePage extends State<ExercisePage> {
           enableCaption: false,
           isLive: false,
           autoPlay: false,
-        )
-    );
+        ));
   }
 
   @override
@@ -49,46 +47,128 @@ class _ExercisePage extends State<ExercisePage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text(
-        ' Exercise',
-        style: TextStyle(color: Colors.white),
-    ),
-    ),
-    body:StaggeredGridView.count(
-    crossAxisCount: 2,
-    crossAxisSpacing: 12.0,
-    mainAxisSpacing: 12.0,
-    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    children: <Widget>[
-    YoutubePlayerBuilder(
-    player: YoutubePlayer(
-    controller: _controller,
-    ),
-    builder: (context, player){
-    return Scaffold(
-    body: Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: [
-    player,
-    SizedBox(height: 10,),
+          title: Text(
+            ' Exercise',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: StaggeredGridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 40.0,
+          mainAxisSpacing: 40.0,
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          children: <Widget>[
+            YoutubePlayerBuilder(
+                player: YoutubePlayer(
+                  controller: _controller,
+                  showVideoProgressIndicator: true,
+                  progressColors: ProgressBarColors(
+                    playedColor: Colors.red,
+                    handleColor: Colors.red,
+                  ),
+                ),
+                builder: (context, player) {
+                  return Scaffold(
+                      body: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                        player,
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ]));
+                }),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black26),
+                  borderRadius: BorderRadius.circular(50)),
+              onPressed: () => {},
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "High Score:" + widget.highScore.toString(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black26),
+                  borderRadius: BorderRadius.circular(50)),
+              onPressed: () => {},
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Timer:" ,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black26),
+                  borderRadius: BorderRadius.circular(50)),
+              onPressed: () => {
 
-    ]
-    )
-    );
-    }
-    ) ,
-    ]
+              },
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Exercise Explanation:" ,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black26),
+                  borderRadius: BorderRadius.circular(50)),
+              onPressed: () => {},
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "submit:" ,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          staggeredTiles: [
+            StaggeredTile.extent(2, 200.0),
+            StaggeredTile.extent(1, 100.0),
+            StaggeredTile.extent(1, 100.0),
+            StaggeredTile.extent(2, 150.0),
+            StaggeredTile.extent(2, 50.0),
 
-    ,
-    staggeredTiles: [
-    StaggeredTile.extent(2, 200.0),
-
-    ],
-    ));
+          ],
+        ));
   }
 }
