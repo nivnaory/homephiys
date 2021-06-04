ff20e08a4cb76e8ac18b3c8da6078e279a9a19a5
niv <naoryniv@gmail.com>
2021-06-04 12:28:57 +0300

    create decription page

--- lib/Pages/main.dart
+++ lib/Pages/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:homephiys/Pages/PhysiotherapistScreens/test3.dart';
-import 'package:homephiys/Pages/test.dart';
-import 'package:homephiys/Pages/test2.dart';
 import 'PatientScreens/LoginScreen.dart';
 
 void main() => runApp(MyApp());
